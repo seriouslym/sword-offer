ed6b06150364831464d850aa3b9ebd06db7b251e
jiajie_zhang <jiajie_zhang@intsig.net>
2025-09-26 18:38:33 +0800

    string

--- string/double-point.ts
+++ string/double-point.ts
@@ -1,4 +1,5 @@
 import assert from "node:assert";
+import {del} from "../linkedList/utils";
 
 /**
  * 不含重复字符的最长字串
@@ -56,7 +57,7 @@ const minWindow = (s: string, t: string) => {
         while (l <= r && validCount === tMap.size) {
             const lChar = s[l]
             win.set(lChar, win.get(lChar)! - 1)
-            if (tMap.has(lChar) && win.has(lChar) && win.get(lChar)! < tMap.get(lChar)!) {
+            if (tMap.has(lChar) && (win.get(lChar) || 0) < tMap.get(lChar)!) {
                 validCount--
             }
             if (win.get(lChar) === 0) {
@@ -76,3 +77,48 @@ assert.equal(minWindow("a", "a"), 'a')
 assert.equal(minWindow("cabwefgewcwaefgcf", "cae"), "cwae")
 assert.equal(minWindow("aaaaaaaaaaaabbbbbcdd", "abcdd"), "abbbbbcdd")
 
+
+/**
+ * 字符串中的变位词
+ * lc567
+ * 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的 排列。如果是，返回 true ；否则，返回 false 。
+ * 换句话说，s1 的排列之一是 s2 的 子串
+ */
+
+const checkInclusion = (s1: string, s2: string) => {
+    // 窗口内的元素需要和s1长度相同且满足包含s1的所有字符字符的出现个数也要一致
+    let l = 0, r = 0, validCount = 0
+    const s1Map = Array.from(s1).reduce((acc, cur) => {
+        acc.set(cur, (acc.get(cur) || 0) + 1)
+        return acc
+    }, new Map<string, number>())
+    const win = new Map<string, number>()
+    while (r < s2.length) {
+        const c = s2[r]
+        win.set(c, (win.get(c) || 0) + 1)
+        if (win.get(c) === s1Map.get(c)) {
+            validCount++
+        }
+        while (l <= r && validCount === s1Map.size) {
+            if (r - l + 1 === s1.length) {
+                return true
+            }
+            const lChar = s2[l]
+            win.set(lChar, win.get(lChar)! - 1)
+            if (win.get(lChar) === 0) {
+                win.delete(lChar)
+            }
+            if (s1Map.has(lChar) && (win.get(lChar) || 0) < s1Map.get(lChar)!) {
+                validCount--
+            }
+            l++
+        }
+        r++
+    }
+    return false
+}
+
+assert.equal(checkInclusion("ab", "eidbaooo"), true, 'fail')
+assert.equal(checkInclusion("ab", "eidboaoo"), false, 'fail')
+assert.equal(checkInclusion("a", "ab"), true, 'fail')
+console.log('checkInclusion pass')
